1e7e74b6c772a10a38f329fdfb7ec5be0213080c
Yogendra Singh <yogendra.singh@castingnetworks.com>
2020-06-02 15:06:31 +0530

    REMOVE:Added Icon to remove edge.

--- src/Edges/CustomEdge.js
+++ src/Edges/CustomEdge.js
@@ -1,15 +1,28 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
+
+const RemoveIcon =()=>(
+    // eslint-disable-next-line react/style-prop-object
+    // eslint-disable-next-line jsx-a11y/alt-text
+    <img style="display:block;cursor: pointer; z-index:10;"
+     src="https://lh6.ggpht.com/5I4BgwoxVAZH5vcPXwdjuNQ6Ellx9YCGgOYif7o2rMwJ2X7sCV96CqXy3OG4XCfwwhGm2C4=w20"
+     />
+);
+
+const onConnectionRemove =(event)=>{
+       alert('Connection deleted');
+}
 
 export default function CustomEdge({
   id, sourceX, sourceY, targetX, targetY, label, style = {}
 }) {
   return (
     <>
-      <path id={id} style={style} className="react-flow__edge-path" d={`M ${sourceX},${sourceY}L ${targetX},${targetY}`} />
+      <path id={id} style={style} className="react-flow__edge-path" d={`M ${sourceX},${sourceY}L ${targetX},${targetY}`} fill="url(#img1)" />
       <text>
-        <textPath href={`#${id}`} style={{ fontSize: '12px' }} startOffset="50%" textAnchor="middle">
-          {label}
-        </textPath>
+       <textPath href={`#${id}`} onClick={onConnectionRemove} style={{ fontSize: '12px', cursor:'pointer' }} startOffset="50%" textAnchor="middle">
+           ❌ 
+      </textPath>  
       </text>
     </>
   );
